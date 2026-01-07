b34d6b34fb611ff8d946f6026307390b2fb27052
TrondlGondl <l.trondl@htlkrems.at>
2026-01-07 11:03:49 +0100

    ablauf geändert

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,18 +4,16 @@ set -e
 echo "Warte auf Azure SQL..."
 python wait_for_db.py
 
-sleep 2
-python manage.py makemigrations
-python manage.py migrate
+echo "DB bereit, starte Django..."
 
-echo "Starte Django (uvicorn)..."
+python manage.py migrate
 python -m uvicorn --host 0.0.0.0 --port 8000 PatientPortal.asgi:application
 EXIT_CODE=$?
 
 if [ $EXIT_CODE -ne 0 ]; then
 python - <<EOF
 from PatientPortalApp.error_notifier import sendError
-sendError(Exception("Uvicorn/Django exited mit Code $EXIT_CODE"))
+sendError(Exception("Uvicorn exited mit Code $EXIT_CODE"))
 EOF
 fi
 
